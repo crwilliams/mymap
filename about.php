126ae30a54d0a53eea73a16607628817e7e4d90d
Colin Williams <github@crwilliams.co.uk>
2013-03-13 20:41:39 +0000

    Updated link to my web site.

--- about.php
+++ about.php
@@ -4,7 +4,7 @@ include 'functions.inc.php';
 outputHeader("About this tool", "", "GENERIC", false, false);
 ?>
 <p>
-This tool was created by <?= mklink('http://id.ecs.soton.ac.uk/person/23977', 'Colin R. Williams') ?> in order to assist in the creation of open geographic data.
+This tool was created by <?= mklink('http://www.crwilliams.co.uk', 'Colin R. Williams') ?> in order to assist in the creation of open geographic data.
 </p>
 <p>
 The problem with existing map creation tools (such as <?= mklink('http://maps.google.com', 'Google Maps') ?> and <?= mklink('http://openspace.ordnancesurvey.co.uk/openspace/', 'OS OpenSpace') ?>) is that their licenses place tight restrictions on what can be done with data created using those tools.  (For more information, see <?= mklink('http://blogs.ecs.soton.ac.uk/data/2011/06/23/generating-open-geographic-data/', 'my blog post', 'blog post on generating open geographic data') ?> on the matter.)  In contrast, this tool makes use of the <?= mklink('http://www.ordnancesurvey.co.uk/oswebsite/products/os-streetview/index.html', 'OS Street View') ?> maps which are provided as part of the <?= mklink('http://www.ordnancesurvey.co.uk/oswebsite/products/os-opendata.html', 'OS OpenData') ?> service, under the <?= mklink('http://www.ordnancesurvey.co.uk/opendata/licence', 'OS OpenData Licence') ?>.  As such, data produced using this tool is considered to be derived data, which is free to be used with attribution to the Ordnance Survey.
